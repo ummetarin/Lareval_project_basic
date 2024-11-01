8f37c807fc9d0e9b5fa1ed63ab767496ae13a509
ummetarin <ummehomairatarin@gmail.com>
2024-11-01 16:14:24 +0600

    Post

--- database/migrations/2024_11_01_092604_create_posts_table.php
+++ database/migrations/2024_11_01_092604_create_posts_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name');
+            $table->text('description');
+            $table->string('image');
         });
     }
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,9 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Document</title><link href="https://cdn.jsdelivr.net/npm/daisyui@4.12.14/dist/full.min.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp,container-queries"></script>
+    <style type="text/tailwindcss">
+    @layer utilities {
+          .cont{
+             @apply px-10 mx-auto py-10 ;
+        }
+      
+    }
+  </style>
 </head>
 <body>
-    <h2>Home</h2>
+  <div class="cont flex flex-row justify-between ">
+  <h2 class="text-green-700">Home</h2>
+  <button class="btn btn-active btn-secondary"><a href="create">Add New Post</a></button>
+  </div>
 </body>
 </html>
